4cfe430da9fcb9371b7914e53fe76a44a975731d
Мамскова Маргарита Дмитриевна <ri57s10@awp11.csc.local>
2016-04-05 10:38:08 +0600

    Add main file

---
+++ cvad.c
@@ -0,0 +1,13 @@
+int main()
+
+{
+
+    int  a, b, c;
+    printf ("Enter a, b, c:");
+    scanf ("%d%d%d", &a, &b, &c);
+    if (a == 0)
+        printf("no solution");
+        dis(a, b, c);
+        system ("pause");
+}
+
