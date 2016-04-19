d71a3dcb1a58aac549fcbed3879a8f6926a70239
Мамскова Маргарита Дмитриевна <ri57s10@awp11.csc.local>
2016-04-19 10:10:36 +0600

    cvad

--- cvad.c
+++ cvad.c
@@ -1,13 +1,22 @@
-int main()
+#include <stdio.h>
+#include "func.h"
+
+int main ()
 
 {
-
-    int  a, b, c;
+	double a, b, c, x1 = 0, x2 = 0;
+	int r;
     printf ("Enter a, b, c:");
-    scanf ("%d%d%d", &a, &b, &c);
-    if (a == 0)
-        printf("no solution");
-        dis(a, b, c);
-        system ("pause");
+    scanf ("%lf%lf%lf", &a, &b, &c);
+    
+    r = func(a, b, c, &x1, &x2);
+	if (r == NOROOTS)
+		printf("\nNo roots");
+	if (r == BAD_ARG)
+		printf("\nInvalid arguments");
+	if (r == ONEROOT)
+		printf("\nx = %2.lf", x1);
+	if (r == TWOROOTS)
+		printf("\nx1 = %2.lf, x2 = %2.lf", x1, x2);
+	return 0;
 }
-
--- function.c
+++ function.c
@@ -1,25 +1,31 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include "func.h"
 
-int dis(int a, int b, int c)
+int  dis (double a, double b, double c, double *x1, double *x2)
 {
-    int D, x1, x2, x;
-     D=b*b-4*a*c;
-            if(D>0)
-            {
-                x1=(-b + sqrt(D)) / (2 * a);
-                x2=(-b - sqrt(D)) / (2 * a);
-                printf("%d %d\n", x1, x2);
-            }
-             else if(D==0)
-            {
-                x=(-b/2*a);
-                printf ("%d\n", x);
-            }
-            else
-            printf("no roots\n");
-            }
-            return 0;
-
+	double d;
+	d = b * b - 4 * a * c;
+	if (a == 0 && b == 0 )
+	{
+		return BAD_ARG;
+	}
+	if (a == 0)
+	{
+		*x1 = -c / b;
+		return ONEROOT;
+	}
+	if (d == 0)
+	{
+		*x1 = -b / (2 * a);
+		return ONEROOT;
+    }
+	if (d < 0)
+	{
+    return ROOT_NO;
+    }
+	*x1 = (-b + sqrt(d)) / (2 * a);
+	*x2 = (-b - sqrt(d)) / (2 * a);
+	return TWOROOTS;
 }
